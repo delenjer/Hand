23d06a366c15055f85874664fb55ba72e14ffe3a
stanislav.mokhyr <stanislav.mokhyr@idapgroup.com>
2020-10-21 11:38:41 +0300

    Updates

---
+++ precache-manifest.ed2742b884f14ff5d38a145aaf26e953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eedaa8124cfe39bfd296360c4dfa90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa8f5adf03c1e17c4b0d",
+    "url": "./static/css/2.c208fc1a.chunk.css"
+  },
+  {
+    "revision": "1b4290a95fa9bd6f932f",
+    "url": "./static/css/main.36e13e74.chunk.css"
+  },
+  {
+    "revision": "aa8f5adf03c1e17c4b0d",
+    "url": "./static/js/2.e6514717.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.e6514717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4290a95fa9bd6f932f",
+    "url": "./static/js/main.6196e77b.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
